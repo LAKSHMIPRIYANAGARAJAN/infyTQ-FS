928c385d774111bc4ca6965c0d0d228aef5fe817
LAKSHMIPRIYANAGARAJAN <47146230+LAKSHMIPRIYANAGARAJAN@users.noreply.github.com>
2019-05-14 08:30:17 +0530

    Create merge_and_sort.py

---
+++ merge_and_sort.py
@@ -0,0 +1,13 @@
+#PF-Exer-29
+def merge_lists(list1,list2):
+    #Write your logic here
+    new_merge_list=list1+list2
+    return new_merge_list
+def sort_list(merged_list):
+    #Write your logic here
+    merged_list.sort()
+    return merged_list
+merged_list=merge_lists(list1=[1,2,3,4,1] ,list2=[2,3,4,5,4,6])
+print(merged_list)
+sorted_merged_list=sort_list(merged_list)
+print(sorted_merged_list)
